c0bb4558e2d1da6b0a64338d2aa8949e313ed1c7
suicidepills <atom.mac@gmail.com>
2014-04-26 18:40:17 -0400

    Add map/mini-map debug code to main.js.
    Signed-off-by: suicidepills <atom.mac@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
 require.config({
-    baseUrl: "js"
+    baseUrl: "js",
 });
+
 define(
 // Requirements
-["map", "mini-map", "first-person", "lib/jquery", "lib/undo-manager"],
+["map", "mini-map", "first-person", "lib/undo-manager", "lib/jquery"],
 
 // Module definition
-function (Map, MiniMap, FirstPerson, $, UndoManager) {
+function (Map, MiniMap, FirstPerson, UndoManager) {
     // TODO
+    
+    var map = new Map();
+    var miniMap = new MiniMap({
+        container: $( $(".mini-map")[0] ),
+        viewRadius: 5,
+        cellSize: 10
+    });
+    
+    miniMap.draw(25, 25, map);
 });
