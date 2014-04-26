5af46a0def9d1ec4bc585a6fde42e0c851ec1535
suicidepills <atom.mac@gmail.com>
2014-04-26 14:19:17 -0400

    Add deps for map.js, add some init/stub code.
    Signed-off-by: suicidepills <atom.mac@gmail.com>

--- js/map.js
+++ js/map.js
@@ -1,8 +1,50 @@
 define(
 // Requirements
-[],
+["lib/event-emitter", "lib/jquery", "lib/rot"],
 
 // Module definition
-function () {
+function (EventEmitter, $) {
+    
+var Map = function (config) {
+    var defaultConfig = {
+        width : 50,
+        height: 50,
+        generator: ROT.Map.DividedMaze
+    };
+    
+    // Merge default config with defaults.
+    config = $.extend({}, defaultConfig, config);
+    
+    // Set up events.
+    var eventer = new EventEmitter();
+    self.bind      = eventer.bind;
+    self.unbind    = eventer.unbind;
+    self.trigger   = eventer.trigger;
+    self.triggerAs = eventer.triggerAs;
+    
+    var _tiles,
+        _playerPosition;
+    
+    
+    /**
+     * Initializes the map data structure and prepares it to be populated with
+     * actual map data.
+     *
+     * @private
+     */
+    function _initMap (width, height) {
+        // TODO
+    }
+    
+    /**
+     * A callback for the map generation algorithm.  Called for each tile as it
+     * is created.
+     *
+     * @private
+     */
+    function _populateMap (x, y, type) {
+        // TODO
+    }
+}
     
 });
