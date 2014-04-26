e1702144dd0495a8c5e2e6d5d158a6f3856ef584
suicidepills <atom.mac@gmail.com>
2014-04-26 14:56:44 -0400

    Add tile.js.
    Signed-off-by: suicidepills <atom.mac@gmail.com>

---
+++ js/tile.js
@@ -0,0 +1,27 @@
+define(
+// Requirements
+["lib/jquery"],
+
+// Module definition
+function () {
+
+var Tile = function (config) {
+    var defaultConfig = {
+        // Position of the tile in the map.
+        x: 0,
+        y: 0,
+        // Whether or not the tile can be passed through.
+        passable : false,
+        // Whether or not the player has seen the given tile.
+        seen     : false,
+        // Whether or not the player is aware of this tile.
+        discoverd: false
+    },
+    self;
+    
+    config = $.extend(self, defaultConfig, config);
+}
+
+return Tile;
+
+});
