f67e7c151bf2f8572bf490be44e58e232f964720
suicidepills <atom.mac@gmail.com>
2014-04-26 16:39:28 -0400

    Move bearing defs to module.
    Signed-off-by: suicidepills <atom.mac@gmail.com>

---
+++ js/lib/bearings.js
@@ -0,0 +1,17 @@
+define(
+// Requirements
+[],
+
+// Module definition
+function () {
+
+// Return an Object with a list of bearing definitions in the following form:
+// ex: [xDelta, yDelta]
+return {
+    NORTH : [ 0  -1],
+    EAST  : [ 1,  0],
+    SOUTH : [ 0, -1],
+    WEST  : [-1,  0]
+};
+
+});
